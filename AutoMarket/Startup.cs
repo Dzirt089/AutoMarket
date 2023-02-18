fd2e61ada36e0994b364e94a18b7b03f9beae3a1
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2023-02-18 22:01:04 +0300

    Добавляем в пулл сервисов, наше подключение к БД

--- AutoMarket/Startup.cs
+++ AutoMarket/Startup.cs
@@ -1,6 +1,8 @@
+using AutoMarket.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace AutoMarket
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
