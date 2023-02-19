7cff0695b0639529148af7d9f853ad89f5bce47a
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2023-02-19 16:25:37 +0300

    Создание класса и интерфейса Репозитоий, для  реализации Интерфейса по управлению добавления, получения, удаления данных из нашей таблицы Car в БД

---
+++ AutoMarket.DAL/Repositories/CarRepository.cs
@@ -0,0 +1,46 @@
+﻿using AutoMarket.DAL.Interfaces;
+using AutoMarket.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.DAL.Repositories
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Create(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Car>> GetAll()
+        {
+            return _context.Car.ToListAsync();
+        }
+
+        public Car GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
