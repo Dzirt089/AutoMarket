de7d18f2be9fa9ec4c705ee0337f2477f2cdd821
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2023-03-05 22:02:50 +0300

    Добавил Интерфейс и реализацию интерфейса для аккаунта. Осталось реализовать Контроллер

---
+++ AutoMarket.Service/Implementations/AccountService.cs
@@ -0,0 +1,106 @@
+﻿using AutoMarket.DAL.Interfaces;
+using AutoMarket.Domain.Entity;
+using AutoMarket.Domain.Enum;
+using AutoMarket.Domain.Response;
+using AutoMarket.Domain.ViewModel.Account;
+using AutoMarket.Service.Interfaces;
+using AutoMarket.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.Service.Implementations
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountService _accountService;
+        private readonly IBaseRepository<User> _userRepository;
+
+        public AccountService(IAccountService accountService,IBaseRepository<User> userRepository)
+        {
+            _accountService = accountService;
+            _userRepository = userRepository;
+        }
+
+        public async Task<BaseResponse<ClaimsIdentity>> Login(LoginViewModel model)
+        {
+            try
+            {
+                var user = await _userRepository.GetAll().FirstOrDefaultAsync(x=>x.Name == model.Name);
+                if (user == null)
+                {
+                    return new BaseResponse<ClaimsIdentity>()
+                    {
+                        Description = "Пользовательне найден"
+                    };
+                }
+                if (user.Password != Hach_md5.HashPassword(model.Password))
+                {
+                    return new BaseResponse<ClaimsIdentity>()
+                    {
+                        Description = "Неверный пароль или логин"
+                    };
+                }
+                var result = Authenticate(user);
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Data = result,
+                    StatusCode = Domain.Enum.StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<ClaimsIdentity> { Description = ex.Message, StatusCode = Domain.Enum.StatusCode.InternalServerError };
+            }
+        }
+
+        public async Task<BaseResponse<ClaimsIdentity>> Register(RegisterViewModel model)
+        {
+            try
+            {
+                var user = await _userRepository.GetAll().FirstOrDefaultAsync(x=>x.Name == model.Name);
+                if(user != null)
+                {
+                    return new BaseResponse<ClaimsIdentity>()
+                    {
+                        Description = "Данный пользователь уже существует"
+                    };
+                }
+                user = new User()
+                {
+                    Name = model.Name,
+                    Role = Role.User,
+                    Password = Hach_md5.HashPassword(model.Password)
+                };
+                await _userRepository.Create(user);
+
+                var result = Authenticate(user);
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Data = result,
+                    Description = "Пользователь добавлен!",
+                    StatusCode = Domain.Enum.StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<ClaimsIdentity> { Description = ex.Message, StatusCode = Domain.Enum.StatusCode.InternalServerError };
+            }
+        }
+
+        private ClaimsIdentity Authenticate(User user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Name),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.ToString())
+            };
+            ClaimsIdentity claimsIndentity = new ClaimsIdentity(claims, "ApplicationCookies", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            return claimsIndentity;
+        }
+    }
+}
---
+++ AutoMarket.Service/Interfaces/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMarket.Domain.Response;
+using AutoMarket.Domain.ViewModel.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.Service.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<BaseResponse<ClaimsIdentity>> Register(RegisterViewModel model);
+        Task<BaseResponse<ClaimsIdentity>> Login (LoginViewModel model);
+    }
+}
