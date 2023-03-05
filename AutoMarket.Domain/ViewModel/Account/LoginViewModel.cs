371d084787852916e418f95433ba8780bfd41881
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2023-03-05 21:05:38 +0300

    Добавление моделей для представления, для страниц Авторизиции и Регистрации

---
+++ AutoMarket.Domain/ViewModel/Account/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoMarket.Domain.ViewModel.Account
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        [MaxLength(20, ErrorMessage = "Имя должно иметь длину меньше 20-ти символов")]
+        [MinLength(3, ErrorMessage = "Имя должно иметь длину более 3-х символов")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
---
+++ AutoMarket.Domain/ViewModel/Account/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoMarket.Domain.ViewModel.Account
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        [MaxLength(20, ErrorMessage = "Имя должно быть в длину не более 20-ти символов")]
+        [MinLength(3, ErrorMessage = "Имя должно быть в длину не менее 3-х символов")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль!")]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage = "Пароль должен быть в длину не менее 6-ти символов")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Подтвердите Ваш пароль")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string PasswordConfirm { get; set; }
+    }
+}
