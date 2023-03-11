0a6b92169901a926395abe41e1dab98eff409762
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2023-03-11 19:19:36 +0300

    Новый фильтр для проверки валидации

---
+++ AutoMarket.Domain/Filter/ValidateModelAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.Domain.Filter
+{
+    /// <summary>
+    /// Фильтр действий для валидации ModelStateя наследую от абстрактного класса ActionFilterAttribute. 
+    /// Он реализует интерфейсы IActionFilter и IResultFilter, а также их асинхронные аналоги, 
+    /// поэтому вы можете переопределить нужные вам 
+    /// методы по мере необходимости.Это позволяет избежать необходимости добавлять неиспользуемый метод OnActionExecuted(), но
+    /// использование базового класса совершенно необязательно. Это вопрос предпочтения
+    /// </summary>
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Переопределяет метод Executing для запуска фильтра до выполнения действия
+        /// Если модель невалидна, задаем свойство Result; 
+        /// это приводит к прерыванию выполнения действия
+        /// </summary>
+        /// <param name="context"></param>
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+
+    }
+}
