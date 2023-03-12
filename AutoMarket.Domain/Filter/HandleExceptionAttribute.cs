d3d6cf4216afd3e609e67f27aef14585e0e661c7
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2023-03-12 19:46:18 +0300

    Добавил фильтр исключений: Фильтры исключений могут перехватывать исключения не только из методов действий и обработчиков страниц. Они будут запускаться, если исключение произойдет:  во время привязки или валидации модели;  когда выполняется метод действия или обработчик страницы;  когда выполняется фильтр действий или фильтр страниц.

---
+++ AutoMarket.Domain/Filter/HandleExceptionAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.Domain.Filter
+{
+    /// <summary>
+    /// ExceptionFilterAttribute – это абстрактный базовый класс, реализующий интерфейс IExceptionFilter
+    /// </summary>
+    public class HandleExceptionAttribute : ExceptionFilterAttribute
+    {
+        /// <summary>
+        /// Для IExceptionFilter можно переопределить только один метод.
+        /// </summary>
+        /// <param name="context"></param>
+        public override void OnException(ExceptionContext context)
+        {
+            //Создание объекта с подробностями о проблеме, чтобы вернуть его в ответе.
+            var error = new ProblemDetails
+            {
+                Title = "An error occurred",
+                Detail = context.Exception.Message,
+                Status = 500,
+                Type = "https://httpstatuses.com/500"
+            };
+            //Создает ObjectResult для сериализации ProblemDetails и установки кода состояния ответа
+            context.Result = new ObjectResult(error) { StatusCode = 500 };
+
+            //Помечает исключение как обработанное, чтобы предотвратить его распространение в конвейер промежуточного ПО.
+            context.ExceptionHandled = true;
+        }
+    }
+}
